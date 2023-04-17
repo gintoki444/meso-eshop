7bdcfe3f3fda0519b915524170bcf73645a214b6
gintoki444 <nugin2010@gmail.com>
2023-04-17 10:36:03 +0700

    สร้าง url path api ของเรา ด้วย env

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+const api = process.env.API_URL;
 
-app.get('/', (req, res)=>{
+
+//http://localhost:3000/api/v1/product
+
+app.get(api+'/product', (req, res)=>{
     res.send('Hello API !');
 }) 
 
 
 app.listen(3000, ()=>{
+    console.log(api);
     console.log("Server is running http://localhost:3000");
 });
